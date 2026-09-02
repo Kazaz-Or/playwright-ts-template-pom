1d7eec4cf3e56fa9c3efb0f9373e61cf216156e1
Kazaz-Or <kazazor@gmail.com>
2026-09-02 22:21:11 +0300

    prettier, update actions

--- utils/logger.ts
+++ utils/logger.ts
@@ -4,7 +4,9 @@ const logger = createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: format.combine(
     format.timestamp({ format: 'HH:mm:ss' }),
-    format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}] ${message}`),
+    format.printf(
+      ({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}] ${message}`,
+    ),
   ),
   transports: [new transports.Console()],
 });
